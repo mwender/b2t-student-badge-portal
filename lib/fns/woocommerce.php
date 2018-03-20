9c2ead0e4e8e9cc5e48dff6949b02746d0054d58
Michael Wender <michael@michaelwender.com>
2018-03-20 11:01:18 -0400

    Updating My Account subnav order

--- lib/fns/woocommerce.php
+++ lib/fns/woocommerce.php
@@ -10,12 +10,25 @@ namespace B2TBadges\fns\woocommerce;
  * @return     array  Ammended nav items array
  */
 function add_tabs( $items ){
-    $items = array_slice( $items, 0, 2, true ) + [ 'classes' => __( 'Classes', 'b2t-student-badge-tabs' ) ] + [ 'certification' => __( 'Certification Program', 'b2t-student-badge-tabs' ) ] + array_slice( $items, 2, null, true ) ;
+  $items = array_slice( $items, 0, 2, true ) + [ 'classes' => __( 'Classes', 'b2t-student-badge-tabs' ) ] + [ 'certification' => __( 'Certification Program', 'b2t-student-badge-tabs' ) ] + array_slice( $items, 2, null, true ) ;
 
-    foreach ($items as $key => $value) {
-        $items[$key] = ucwords( $value );
-    }
+  foreach ($items as $key => $value) {
+    $items[$key] = ucwords( $value );
+  }
 
-    return $items;
+  $new_items = [
+    'dashboard' => $items['dashboard'],
+    'certification' => $items['certification'],
+    'classes' => $items['classes'],
+    'orders' => $items['orders'],
+    'subscriptions' => $items['subscriptions'],
+    'downloads' => $items['downloads'],
+    'edit-account' => $items['edit-account'],
+    'edit-address' => $items['edit-address'],
+    'payment-methods' => $items['payment-methods'],
+    'customer-logout' => $items['customer-logout'],
+  ];
+
+  return $new_items;
 }
-add_filter( 'woocommerce_account_menu_items', __NAMESPACE__ . '\\add_tabs' );
+add_filter( 'woocommerce_account_menu_items', __NAMESPACE__ . '\\add_tabs', 20 );
