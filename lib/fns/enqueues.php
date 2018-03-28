40373f0530d848fb638cb8ecf3f4053402c1bee1
Michael Wender <michael@michaelwender.com>
2018-03-28 16:39:21 -0400

    Layout/Display edits

--- lib/fns/enqueues.php
+++ lib/fns/enqueues.php
@@ -5,7 +5,9 @@ namespace B2TBadges\fns\enqueues;
 function enqueue_scripts(){
     if( \b2t_is_endpoint( 'certification' ) || \b2t_is_endpoint( 'classes' )  ){
         wp_dequeue_script( 'divi-custom-script' );
+
         wp_register_script( 'handlebars', BADGE_PORTAL_PLUGIN_URL . 'lib/js/handlebars-v4.0.5.js', null, filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/handlebars-v4.0.5.js' ), true );
+        wp_register_script( 'handlebars-intl', BADGE_PORTAL_PLUGIN_URL . 'lib/js/handlebars-intl.min.js', ['handlebars'], filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/handlebars-intl.min.js'), true );
 
         $current_user = wp_get_current_user();
         $student_id = get_user_meta( $current_user->ID, 'sf_student_id', true );
@@ -35,7 +37,7 @@ function enqueue_scripts(){
     }
     if( \b2t_is_endpoint( 'certification' ) ){
         wp_register_script( 'open-badges-issuer', 'https://backpack.openbadges.org/issuer.js', null, null, true );
-        wp_enqueue_script( 'b2t-badges-tab', BADGE_PORTAL_PLUGIN_URL . 'lib/js/badges-tab.js', ['jquery','handlebars','open-badges-issuer','jquery-ui-tabs'], filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/badges-tab.js' ), true );
+        wp_enqueue_script( 'b2t-badges-tab', BADGE_PORTAL_PLUGIN_URL . 'lib/js/badges-tab.js', ['jquery','handlebars','handlebars-intl','open-badges-issuer','jquery-ui-tabs'], filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/badges-tab.js' ), true );
 
         wp_localize_script( 'b2t-badges-tab', 'wpvars', [
             'jsonurl' => site_url( 'wp-json/' . BADGE_API_NAMESPACE . '/sf/' ),
@@ -50,7 +52,7 @@ function enqueue_scripts(){
 
     }
     if( \b2t_is_endpoint( 'classes' ) ){
-        wp_register_script( 'handlebars-intl', BADGE_PORTAL_PLUGIN_URL . 'lib/js/handlebars-intl.min.js', ['handlebars'], filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/handlebars-intl.min.js'), true );
+
         wp_enqueue_script( 'b2t-classes-tab', BADGE_PORTAL_PLUGIN_URL . 'lib/js/classes-tab.js', ['jquery','handlebars','handlebars-intl'], filemtime( BADGE_PORTAL_PLUGIN_PATH . 'lib/js/classes-tab.js' ), true );
 
         wp_localize_script( 'b2t-classes-tab', 'wpvars', [
