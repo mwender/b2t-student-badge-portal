fac06e26f2aa5fb7567d80608268fca3e99eb660
Michael Wender <michael@michaelwender.com>
2019-09-04 16:33:52 -0400

    Removing old Github updates code, moving session start to top of file

--- b2t-student-badge-tabs.php
+++ b2t-student-badge-tabs.php
@@ -11,46 +11,17 @@
  *
  * @package         B2t_Student_Badge_Tabs
  */
+
+ // We store SalesForce session credentials in $_SESSION['SF_SESSION']
+if( ! headers_sent() )
+  session_start();
+
 define( 'BADGE_PORTAL_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'BADGE_PORTAL_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'BADGE_API_NAMESPACE', 'b2tbadges/v1' );
 
-// We store SalesForce session credentials in $_SESSION['SF_SESSION']
-session_start();
-
 // Load libraries
 require ( BADGE_PORTAL_PLUGIN_PATH . 'lib/typerocket/init.php' );
-
-// Initialize Plugin Updates
-/*
-if( is_admin() ){
-    require_once ( plugin_dir_path( __FILE__ ) . 'lib/classes/plugin-updater.php' );
-    add_action( 'init', function(){
-        // If you're experiencing GitHub API rate limits while testing
-        // plugin updates, create a `Personal access token` under your
-        // GitHub profile's `Developer Settings`. Then add
-        // `define( 'GITHUB_ACCESS_TOKEN', your_access_token );` to
-        // your site's `wp-config.php`.
-        new GitHub_Plugin_Updater( __FILE__, 'mwender', 'b2t-student-badge-portal', GITHUB_ACCESS_TOKEN );
-    } );
-}
-*/
-
-/**
- * Check whether the currently viewed page is an endpoint
- *
- * @param      string  $endpoint  The endpoint
- *
- * @return     boolean  Returns TRUE if is an endpoint.
- */
-function b2t_is_endpoint( $endpoint = false ){
-    global $wp_query;
-
-    if( ! $wp_query )
-        return false;
-
-    return isset( $wp_query->query[ $endpoint ] );
-}
 
 // Include files
 require_once( BADGE_PORTAL_PLUGIN_PATH . 'lib/fns/badge_cpt.php' );
