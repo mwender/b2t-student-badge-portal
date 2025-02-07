e1c27aac74708d68b0e16f03f34f1971afb50404
Michael Wender <michael@michaelwender.com>
2025-02-07 14:12:03 -0500

    Pulling from an ACF Group > Subfield

--- README.md
+++ README.md
@@ -3,13 +3,16 @@
 **Tags:** wordpress,woocommerce,salesforce
 **Requires at least:** 6.1.0
 **Tested up to:** 6.7
-**Stable tag:** 1.3.0
+**Stable tag:** 1.3.1
 **License:** GPLv2 or later
 **License URI:** http://www.gnu.org/licenses/gpl-2.0.html
 
 A WordPress plugin which provides a listing of B2T Student Badges/Certificates inside a user's WooCommerce account pages.
 
 ## Changelog
+
+### 1.3.1
+* Updating Certification and Classes text content to pull from an ACF Group > Subfield.
 
 ### 1.3.0
 * Converting Certification and Classes tab content to be pulled from an ACF Options Page field.
--- lib/fns/content.php
+++ lib/fns/content.php
@@ -12,8 +12,8 @@ function classes_endpoint_content() {
 <h2>Classes/Exams</h2>
 <?php
 global $post;
-$content = get_field( 'class_exams_tab', 'option' );
-echo ( $content )? apply_filters( 'the_content', $content ) : '<pre><strong>MISSING CONTENT</strong><br/>Add content here by editing the "Class Exams Tab" field for this page. [<a href="' . get_edit_post_link( $post->ID ) . '">Edit</a>]</pre>';
+$content = get_field( 'my_account_class_exams_tab', 'option' );
+echo ( $content )? apply_filters( 'the_content', $content ) : '<pre><strong>MISSING CONTENT</strong><br/>Add content here by editing the "Class Exams Tab" field under B2T Text Fields.</pre>';
 ?>
 <div id="classes">
   <table class="classes">
@@ -43,8 +43,8 @@ function certification_endpoint_content() {
 <?php
 global $post;
 $content = apply_filters( 'the_content', get_post_meta($post->ID, 'certification_program_tab', true) );
-$content = get_field( 'certification_program_tab', 'option' );
-echo ( $content )? $content : '<pre><strong>MISSING CONTENT</strong><br/>Add content here by editing the "Certification Program Tab" field for this page. [<a href="' . get_edit_post_link( $post->ID ) . '">Edit</a>]</pre>';
+$content = get_field( 'my_account_certification_program_tab', 'option' );
+echo ( $content )? $content : '<pre><strong>MISSING CONTENT</strong><br/>Add content here by editing the "Certification Program Tab" field under B2T Text Fields</pre>';
 ?>
 <div id="tabs">
     <ul>
