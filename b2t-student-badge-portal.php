33da511daef8d910b551f5b6351edcf7448fca79
Michael Wender <michael@michaelwender.com>
2026-02-03 18:37:38 -0500

    Adding DB table

--- b2t-student-badge-portal.php
+++ b2t-student-badge-portal.php
@@ -37,3 +37,38 @@ require_once( BADGE_PORTAL_PLUGIN_PATH . 'lib/fns/student-resources.rest-api.php
 require_once( BADGE_PORTAL_PLUGIN_PATH . 'lib/fns/utilities.php' );
 require_once( BADGE_PORTAL_PLUGIN_PATH . 'lib/fns/woocommerce.php' );
 require_once( BADGE_PORTAL_PLUGIN_PATH . 'lib/fns/zoho.php' );
+
+/**
+ * Create the badge assertions table.
+ *
+ * @return void
+ */
+function b2t_create_badge_assertions_table() {
+  global $wpdb;
+
+  $table_name      = $wpdb->prefix . 'b2t_badge_assertions';
+  $charset_collate = $wpdb->get_charset_collate();
+
+  $sql = "
+    CREATE TABLE {$table_name} (
+      assertion_id CHAR(64) NOT NULL,
+      recipient_hash CHAR(64) NOT NULL,
+      badge_slug VARCHAR(190) NOT NULL,
+      issued_on DATE NOT NULL,
+      assertion_json LONGTEXT NOT NULL,
+      created_at DATETIME NOT NULL,
+      revoked_at DATETIME NULL,
+      PRIMARY KEY (assertion_id),
+      KEY badge_slug (badge_slug),
+      KEY issued_on (issued_on)
+    ) {$charset_collate};
+  ";
+
+  require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+  dbDelta( $sql );
+}
+
+register_activation_hook(
+  __FILE__,
+  'b2t_create_badge_assertions_table'
+);
